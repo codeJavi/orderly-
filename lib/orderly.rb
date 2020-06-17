59a23b80d999cc8f056da60f80a74f8a3a9be8db
Javi Leyva <jvrleyva@gmail.com>
2020-06-17 15:04:57 +0000

    modified orderly project

--- lib/orderly.rb
+++ lib/orderly.rb
@@ -1,6 +1,8 @@
-require "orderly/version"
 
 module Orderly
   class Error < StandardError; end
   # Your code goes here...
 end
+
+require_relative "./orderly/version"
+require_relative "./orderly/cli"
