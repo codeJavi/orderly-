1b300806204412e0795a70154366294123a4a09e
Javi Leyva <jvrleyva@gmail.com>
2020-06-18 02:02:00 +0000

    added more methods to 'cli' file

--- lib/orderly/cli.rb
+++ lib/orderly/cli.rb
@@ -1,9 +1,37 @@
 class Orderly::CLI
   
-  def greet 
-    puts "Hello! Where Can we Plan Your Next Vacation?:"
+  def call  
+    list top_deals 
+    menu 
+    
   end 
   
+  def list top_deals
+    # here doc 
+    puts "Hello! Are you ready to book your next Cruise?:"
+    
+  1. #list deals for rc 
+  2. #list deals for virgin
+  3. #list deals for Norweigan 
+  end 
+ 
+ def menu 
+   puts "Enter the date of trip"
+   input = gets. strip 
+   case input 
+   when "1"
+      puts "More info on deal 1..."
+  when "2"
+    puts "More info on deal 2..."
+  when "3"
+    puts "more info on deal 3..."
+  end 
 end 
-  
+    
+ end 
+ 
+ 
+ 
+ 
+ 
   
